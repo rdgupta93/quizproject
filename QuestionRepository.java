90c9f5eda44ea473dd31f4887f8f38b0b969aedf
rdgupta93 <110840567+rdgupta93@users.noreply.github.com>
2024-12-20 01:37:23 +0530

    Add files via upload

---
+++ QuestionRepository.java
@@ -0,0 +1,23 @@
+package com.quize.quizproject.repository;
+
+import com.quize.quizproject.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question,Integer> {
+    List<Question> findByCategory(String category);
+
+//    @Query(value = "SELECT * FROM question q where q.category=:category ORDER BY RANDOM() LIMIT:numQ",nativeQuery = true)
+//    List<Question> findRandomQuestionByCategory(String category, Integer numQ);
+
+    @Query(value = "SELECT * FROM question q WHERE q.category = :category ORDER BY RAND() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionByCategory(@Param("category") String category, @Param("numQ") Integer numQ);
+
+    @Query("SELECT q.questionId, q.rightAns FROM Question q WHERE q.id IN :questionIds")
+    List<Object[]> findCorrectAnswersByQuestionIds(@Param("questionIds") List<Integer> questionIds);
+}
